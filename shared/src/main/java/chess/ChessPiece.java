165d84c780cb3f2e943ce47d747acb69ee74e777
bstan218 <bstan218@gmail.com>
2024-05-06 20:49:34 -0600

    implemented getPieceType() and getTeamColor

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -10,10 +10,12 @@ import java.util.Collection;
  */
 public class ChessPiece {
 
-    public PieceType type;
-    public ChessPosition position;
+    private PieceType type;
+    private ChessGame.TeamColor pieceColor;
 
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.type = type;
+        this.pieceColor = pieceColor;
     }
 
     /**
@@ -32,14 +34,14 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return type;
     }
 
     /**
