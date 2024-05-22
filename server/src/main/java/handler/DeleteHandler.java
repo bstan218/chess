eee917dc7257b51ec41973cd922eb093e2d13a08
bstan218 <bstan218@gmail.com>
2024-05-22 11:07:39 -0600

    started implementing DeleteHandler

---
+++ server/src/main/java/handler/DeleteHandler.java
@@ -0,0 +1,10 @@
+package handler;
+
+import com.google.gson.GsonBuilder;
+import spark.*;
+
+public class DeleteHandler {
+    public Object handleRequest(Request req, Response res) {
+        return "";
+    }
+}
--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,8 +1,16 @@
 package server;
 
 import spark.*;
+import handler.*;
 
 public class Server {
+    private static DeleteHandler deleteHandler;
+    private static RegisterHandler registerHandler;
+
+    public Server() {
+        deleteHandler = new DeleteHandler();
+        registerHandler = new RegisterHandler();
+    }
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
@@ -10,6 +18,7 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        createRoutes();
 
         Spark.awaitInitialization();
         return Spark.port();
@@ -21,6 +30,9 @@ public class Server {
     }
 
     private static void createRoutes() {
-
+        //Spark.get("/db", (req, res) -> "Hello BYU!");
+        Spark.delete("/db", (req, res) ->
+                deleteHandler.handleRequest(req, res));
+        Spark.post("/user", (req, res))
     }
 }
