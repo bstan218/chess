95ed45b5324e8503407563de97c5ec88ec999347
bstan218 <bstan218@gmail.com>
2024-05-22 12:34:11 -0600

    created Service class stems

---
+++ server/src/main/java/service/GameService.java
@@ -0,0 +1,11 @@
+package service;
+
+import model.AuthData;
+import model.GameData;
+import java.util.ArrayList;
+
+public class GameService {
+    public GameData createGame(AuthData auth, GameData game) {}
+    public ArrayList<GameData> listGames(AuthData auth) {}
+    public void joinGame(AuthData auth, GameData game) {}
+}
---
+++ server/src/main/java/service/UserService.java
@@ -0,0 +1,13 @@
+package service;
+
+import model.*;
+
+public class UserService {
+    public AuthData register(UserData user) {
+
+    }
+    public AuthData login(UserData user) {
+
+    }
+    public void logout(AuthData auth) {}
+}
