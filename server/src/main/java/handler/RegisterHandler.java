d2595f278b4469e38b77fe6b4f2bd6a4cb13948e
bstan218 <bstan218@gmail.com>
2024-05-22 11:11:17 -0600

    started implementing RegisterHandler

---
+++ server/src/main/java/handler/RegisterHandler.java
@@ -0,0 +1,10 @@
+package handler;
+
+import spark.Request;
+import spark.Response;
+
+public class RegisterHandler {
+    public Object handleRequest(Request req, Response res) {
+        return "";
+    }
+}
