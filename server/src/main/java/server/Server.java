5be1aa0c1e4a0695d80ff34a6301b6824ec2f599
bstan218 <bstan218@gmail.com>
2024-05-22 11:30:10 -0600

    pluggled handlers into Server.java, DeleteHandler -> ClearHandler.java

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -4,12 +4,22 @@ import spark.*;
 import handler.*;
 
 public class Server {
-    private static DeleteHandler deleteHandler;
+    private static ClearHandler clearHandler;
     private static RegisterHandler registerHandler;
+    private static LoginHandler loginHandler;
+    private static LogoutHandler logoutHandler;
+    private static ListGameHandler listGameHandler;
+    private static CreateGameHandler createGameHandler;
+    private  static JoinGameHandler joinGameHandler;
 
     public Server() {
-        deleteHandler = new DeleteHandler();
+        clearHandler = new ClearHandler();
         registerHandler = new RegisterHandler();
+        loginHandler = new LoginHandler();
+        logoutHandler = new LogoutHandler();
+        listGameHandler = new ListGameHandler();
+        createGameHandler = new CreateGameHandler();
+        joinGameHandler = new JoinGameHandler();
     }
 
     public int run(int desiredPort) {
@@ -32,7 +42,18 @@ public class Server {
     private static void createRoutes() {
         //Spark.get("/db", (req, res) -> "Hello BYU!");
         Spark.delete("/db", (req, res) ->
-                deleteHandler.handleRequest(req, res));
-        Spark.post("/user", (req, res))
+                clearHandler.handleRequest(req, res));
+        Spark.post("/user", (req, res) ->
+                registerHandler.handleRequest(req, res));
+        Spark.post("/session", (req, res) ->
+                loginHandler.handleRequest(req, res));
+        Spark.delete("/session", (req, res) ->
+                logoutHandler.handleRequest(req, res));
+        Spark.get("/game", (req, res) ->
+                listGameHandler.handleRequest(req, res));
+        Spark.post("/game", (req, res) ->
+                createGameHandler.handleRequest(req, res));
+        Spark.put("/game", (req, res) ->
+                joinGameHandler.handleRequest(req, res));
     }
 }
