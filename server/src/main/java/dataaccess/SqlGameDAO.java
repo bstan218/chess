63ab2be69c60047562e4d00e063a7845839971b8
bstan218 <bstan218@gmail.com>
2024-06-06 20:09:30 -0600

    added createGame and getGameFromName methods for SqlGameDAO

--- server/src/main/java/dataaccess/SqlGameDAO.java
+++ server/src/main/java/dataaccess/SqlGameDAO.java
@@ -21,11 +21,12 @@ public class SqlGameDAO implements GameDAO {
             """
             CREATE TABLE IF NOT EXISTS  game (
               'gameID' int NOT NULL AUTO_INCREMENT,
-              `whiteUsername` varchar(256) NOT NULL,
-              `blackUsername` varchar(256) NOT NULL,
+              `whiteUsername` varchar(256) DEFAULT NULL,
+              `blackUsername` varchar(256) DEFAULT NULL,
               `gameName` varchar(256) NOT NULL,
               `json` TEXT DEFAULT NULL,
-              PRIMARY KEY (`gameID`)
+              PRIMARY KEY (`gameID`),
+              INDEX (gameName)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
             """
     };
@@ -33,7 +34,19 @@ public class SqlGameDAO implements GameDAO {
 
     @Override
     public int createGame(String gameName) throws DataAccessException {
-        return 0;
+        if (gameName == null) throw new DataAccessException("Error: no game name provided");
+        try {
+            getGameFromName(gameName);
+        } catch (DataAccessException e) {
+            var statement = "INSERT INTO game (gameName) VALUES (?)";
+            try {
+                DatabaseManager.executeUpdate(statement, gameName);
+            } catch (Exception ne) {
+                throw new DataAccessException("Error: Unable to insert game into database");
+            }
+
+            return getGameFromName(gameName).gameID();
+        } throw new DataAccessException("Game Name taken");
     }
 
     @Override
@@ -55,6 +68,25 @@ public class SqlGameDAO implements GameDAO {
         }
     }
 
+    private GameData getGameFromName(String gameName) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT gameID, whiteUsername, blackUsername, gameName, json FROM game WHERE gameName=?";
+            try (var ps = conn.prepareStatement(statement)) {
+                ps.setString(1, gameName);
+                try(var rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        return readGame(rs);
+                    } else {
+                        throw new DataAccessException("Error: No game with that gameID");
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
     private GameData readGame(ResultSet rs) throws SQLException {
         var gameID = rs.getInt("gameID");
         var whiteUsername = rs.getString("whiteUsername");
@@ -72,7 +104,11 @@ public class SqlGameDAO implements GameDAO {
 
     @Override
     public void updateGame(int gameID, GameData gameData) throws DataAccessException {
+        try {
+            getGame(gameID);
+        } catch (DataAccessException e) {
 
+        }
     }
 
     @Override
