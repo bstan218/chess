525eee8b7587503393623736da633ae59a50a184
bstan218 <bstan218@gmail.com>
2024-06-06 19:30:44 -0600

    implemented deleteAuth method to SqlAuthDAO

--- server/src/main/java/dataaccess/SqlAuthDAO.java
+++ server/src/main/java/dataaccess/SqlAuthDAO.java
@@ -9,7 +9,7 @@ import java.util.UUID;
 public class SqlAuthDAO implements AuthDAO {
 
     public SqlAuthDAO() {
-        try  {
+        try {
             DatabaseManager.configureDatabase(createStatements);
         } catch (DataAccessException e) {
             throw new RuntimeException(e);
@@ -50,7 +50,7 @@ public class SqlAuthDAO implements AuthDAO {
             var statement = "SELECT authToken, username FROM auth WHERE authToken=?";
             try (var ps = conn.prepareStatement(statement)) {
                 ps.setString(1, authToken);
-                try(var rs = ps.executeQuery()) {
+                try (var rs = ps.executeQuery()) {
                     if (rs.next()) {
                         return readAuth(rs);
                     } else {
@@ -72,6 +72,15 @@ public class SqlAuthDAO implements AuthDAO {
 
     @Override
     public void deleteAuth(String authToken) {
-
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "DELETE FROM auth WHERE authToken=?";
+            try {
+                DatabaseManager.executeUpdate(statement, authToken);
+            } catch (Exception ne) {
+                throw new DataAccessException("Error: Unable to insert user into database");
+            }
+        } catch (SQLException | DataAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
