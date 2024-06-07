a7fc45bc484864cf3ff158b4f2ee89e94c9af3f8
bstan218 <bstan218@gmail.com>
2024-06-06 18:55:10 -0600

    added readAuth and getAuth methods to SqlAuthDAO

--- server/src/main/java/dataaccess/SqlAuthDAO.java
+++ server/src/main/java/dataaccess/SqlAuthDAO.java
@@ -2,6 +2,9 @@ package dataaccess;
 
 import model.AuthData;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class SqlAuthDAO implements AuthDAO {
 
     public SqlAuthDAO() {
@@ -22,8 +25,6 @@ public class SqlAuthDAO implements AuthDAO {
             """
     };
 
-
-
     @Override
     public void deleteAllAuths() throws DataAccessException {
         var statement = "TRUNCATE auth";
@@ -37,8 +38,29 @@ public class SqlAuthDAO implements AuthDAO {
 
     @Override
     public AuthData getAuth(String authToken) throws DataAccessException {
-        return null;
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT authToken, username FROM auth WHERE authToken=?";
+            try (var ps = conn.prepareStatement(statement)) {
+                ps.setString(1, authToken);
+                try(var rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        return readAuth(rs);
+                    } else {
+                        throw new DataAccessException("Error: authorization is invalid");
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
+
+    private AuthData readAuth(ResultSet rs) throws SQLException {
+        var authToken = rs.getString("authToken");
+        var username = rs.getString("username");
+        return new AuthData(authToken, username);
+    }
+
 
     @Override
     public void deleteAuth(String authToken) {
